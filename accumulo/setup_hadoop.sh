47f334fa0593464e02f774b640a8b1b0a60ed8f5
Carlos G. Cruz <carlos.cruz09@gmail.com>
2016-03-09 09:21:49 -0500

    Fixing minor docker issues.

--- accumulo/setup_hadoop.sh
+++ accumulo/setup_hadoop.sh
@@ -4,10 +4,6 @@
 sed -i '/^export JAVA_HOME/ s:.*:'"export JAVA_HOME=$JAVA_HOME\nexport HADOOP_HOME=$HADOOP_HOME\n"':' $HADOOP_HOME/etc/hadoop/hadoop-env.sh
 sed -i '/^export HADOOP_CONF_DIR/ s:.*:'"export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop/"':' $HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
-#Create directories for hdfs files and mapred temporary files
-mkdir -p /data/hadoop
-chown -R hdfs:hadoop /data/hadoop
-
 mkdir $HADOOP_PREFIX/input
 cp $HADOOP_CONF_DIR/*.xml $HADOOP_PREFIX/input
 
