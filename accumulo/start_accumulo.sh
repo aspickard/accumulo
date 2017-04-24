566ff10273e38813e338b7ac5cf31b0f650e19d9
Eric Wimberley <blank89@gmail.com>
2017-04-24 15:48:18 -0400

    Switching to IP address instead of host name.

--- accumulo/start_accumulo.sh
+++ accumulo/start_accumulo.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sed "s/HOSTNAME/$HOSTNAME/g" $ACCUMULO_HOME/conf/accumulo-site-template.xml > $ACCUMULO_HOME/conf/accumulo-site.xml
-sed "s/HOSTNAME/$HOSTNAME/g" $ACCUMULO_HOME/conf/client.conf.template > $ACCUMULO_HOME/conf/client.conf
+ADDRESS="$(hostname -I)"
 
-echo $HOSTNAME > $ACCUMULO_HOME/conf/gc
-echo $HOSTNAME > $ACCUMULO_HOME/conf/masters
-echo $HOSTNAME > $ACCUMULO_HOME/conf/monitor
-echo $HOSTNAME > $ACCUMULO_HOME/conf/slaves
-echo $HOSTNAME > $ACCUMULO_HOME/conf/tracers
+sed "s/HOSTNAME/$ADDRESS/g" $ACCUMULO_HOME/conf/accumulo-site-template.xml > $ACCUMULO_HOME/conf/accumulo-site.xml
+sed "s/HOSTNAME/$ADDRESS/g" $ACCUMULO_HOME/conf/client.conf.template > $ACCUMULO_HOME/conf/client.conf
+
+echo $ADDRESS > $ACCUMULO_HOME/conf/gc
+echo $ADDRESS > $ACCUMULO_HOME/conf/masters
+echo $ADDRESS > $ACCUMULO_HOME/conf/monitor
+echo $ADDRESS > $ACCUMULO_HOME/conf/slaves
+echo $ADDRESS > $ACCUMULO_HOME/conf/tracers
 
 #Start Accumulo
 su -s /bin/bash accumulo -c '$ACCUMULO_HOME/bin/start-all.sh'
